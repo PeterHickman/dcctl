957dcf208a62bbbdd900469986f0c01d71adec1a
Peter Hickman <peterhickman386@googlemail.com>
2024-11-17 21:06:05 +0000

    clean up the build

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 BINARY='/usr/local/bin'
+APP=dcctl
 
-echo "Building dcctl"
-go build dcctl.go
+echo "Building $APP"
+go build -ldflags="-s -w" $APP.go
 
-echo "Installing dcctl to $BINARY"
-install -v dcctl $BINARY
+echo "Installing $APP to $BINARY"
+install -v $APP $BINARY
 
 echo "Removing the build"
-rm dcctl
+rm $APP
